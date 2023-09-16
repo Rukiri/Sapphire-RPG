d5b6124abc13d717928de64745cda0891ba6f686
Gou Shin <esitter172@gmail.com>
2023-09-16 00:33:50 -0500

    smoothness
    object not longer jitters when moving diagonally

--- player.gd
+++ player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
-var move_speed : float = 125
-var move_speed_diag : float = floor(move_speed * ((sqrt(2))/ 2)) + 37 # keeps the speed constant even when moving diagonal
+var move_speed : int = 50
+var move_speed_diag : float = round(move_speed * ((sqrt(2))/ 2)) + 21 # keeps the speed constant even when moving diagonal
 var move_dir: get = _get_input_direction
 	
 func _physics_process(delta):
-	velocity = move_dir * move_speed
+	if move_speed % 2 == 0:
+		global_position = global_position.round()
+	else: # Odd
+		global_position = global_position.floor()
+	velocity = round(move_dir * move_speed)
 	move_and_slide()
+	print(velocity)
 
 func _get_input_direction():
 	var move_x = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
@@ -16,10 +21,7 @@ func _get_input_direction():
 	if move_x && move_y:
 		move_speed = move_speed_diag
 	else:
-		move_speed = 125
-		
-	print(move_speed)
+		move_speed = 50
 
 	move_dir = Vector2(move_x, move_y).normalized()
 	return move_dir
-	
